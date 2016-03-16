bf2e723b40dcf321a3f0408ddc109dd13a6540df
foxlalala <fox-lalala@hotmail.com>
2016-03-17 01:23:12 +0800

    第5次紅燈

--- BookShoppingCartTests/MyShoppingCartTests.cs
+++ BookShoppingCartTests/MyShoppingCartTests.cs
@@ -84,5 +84,26 @@ namespace BookShoppingCart.Tests
             var expected = 320;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void 一次買了整套_一二三四五集各買了一本()
+        {
+            //arrange
+            var target = new List<Book>()
+            {
+                new Book { Name = "HarryPotter1", Price = 100 },
+                new Book { Name = "HarryPotter2", Price = 100 },
+                new Book { Name = "HarryPotter3", Price = 100 },
+                new Book { Name = "HarryPotter4", Price = 100 },
+                new Book { Name = "HarryPotter5", Price = 100 },
+            };
+
+            //act
+            var actual = new MyShoppingCart().CheckOut(target);
+
+            //assert
+            var expected = 375;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
