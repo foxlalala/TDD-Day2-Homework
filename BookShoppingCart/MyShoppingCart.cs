c68d2f6de0a7e84fa5bde077c9a9e72da1aa57aa
foxlalala <fox-lalala@hotmail.com>
2016-03-17 01:12:29 +0800

    第二次綠燈

--- BookShoppingCart/MyShoppingCart.cs
+++ BookShoppingCart/MyShoppingCart.cs
@@ -14,10 +14,20 @@ namespace BookShoppingCart
         public int CheckOut(List<Book> myBookList)
         {
             var totalPrice = 0;
+            var bookType = new List<string>();
+
             foreach (var Book in myBookList)
             {
                 totalPrice += Book.Price;
+
+                if (bookType.Contains(Book.Name))
+                    continue;
+
+                bookType.Add(Book.Name);
             }
+            
+            if (bookType.Count == 2)
+                totalPrice = Convert.ToInt32(totalPrice * 0.95);
 
             return totalPrice;
         }
