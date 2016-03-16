23f4c32adf4d9c5993e96e9503123844fd5c37a0
foxlalala <fox-lalala@hotmail.com>
2016-03-17 00:12:45 +0800

    初始化與第一次紅燈

---
+++ BookShoppingCart/MyShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShoppingCart
+{
+    public class MyShoppingCart
+    {
+        public void BookShopping()
+        {
+
+        }
+    }
+}
---
+++ BookShoppingCartTests/MyShoppingCartTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoppingCart.Tests
+{
+    [TestClass()]
+    public class MyShoppingCartTests
+    {
+        [TestMethod()]
+        public void 第一集買了一本_其他都沒買()
+        {
+            //arrange
+            var target = new List<Book>()
+            {
+                new Book { Name = "HarryPotter1", Price = 100 },
+            };
+
+            //act
+            var actual = CheckOut(target);
+
+            //assert
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+
+        public class Book
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+        public int CheckOut(List<Book> bk)
+        {
+            return 0;
+        }
+    }
+}
